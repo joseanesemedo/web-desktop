4b441151d536525bbb54d477d4b148255a1c0e4a
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-04-12 10:53:42 +0100

    added option to remove crt effect

--- webdesktop/src/App.js
+++ webdesktop/src/App.js
@@ -1,11 +1,20 @@
 import "./App.scss";
 import Toolbar from "./components/Toolbar";
 import Main from "./components/Main";
+import { useState } from "react";
 
 function App() {
+  const [effect, setEffect] = useState(true);
+
   return (
     <div className="app">
-      <div className="overlay"></div>
+      {effect ? (
+        <>
+          <div className="noise"></div>
+          <div className="overlay"></div>
+        </>
+      ) : null}
+
       <Main />
       <Toolbar />
     </div>
