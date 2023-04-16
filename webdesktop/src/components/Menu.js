970b7e6cde45344b7a0f6ab79e096dc0e2a8d10f
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-04-16 11:37:37 +0100

    changed Dropdown to Menu

---
+++ webdesktop/src/components/Menu.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Menu.scss";
+
+const Dropdown = ({ submenus, dropdown }) => {
+  return (
+    <ul className={`menu ${dropdown ? "show" : ""}`}>
+      {submenus.map((submenu, index) => (
+        <li key={index} className="menu__items">
+          <a href={submenu.url}>
+            <img
+              className="menu__item_img"
+              src={submenu.imgURL}
+              alt={submenu.imgAlt}
+            />
+            <span>{submenu.title}</span>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Dropdown;
