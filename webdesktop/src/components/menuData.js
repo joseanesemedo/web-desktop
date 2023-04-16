9603e644e453123a2d5e1b0eb831206644ecb332
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-04-16 11:15:45 +0100

    Changed menuItems to menuData

---
+++ webdesktop/src/components/menuData.js
@@ -0,0 +1,14 @@
+export const menuItems = [
+  {
+    title: "web design",
+    url: "web-design",
+  },
+  {
+    title: "web development",
+    url: "web-dev",
+  },
+  {
+    title: "SEO",
+    url: "seo",
+  },
+];
