5fd26dac98131346983c940076262ad2218c21c4
Joseane Semedo <joseanelmsemedo2@gmail.com>
2023-04-23 12:04:08 +0100

    testing react-powerglitch

--- webdesktop/src/components/Main.js
+++ webdesktop/src/components/Main.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Main.scss";
 import PopupWindow from "./Windows/PopupWindow";
 import MessageWindow from "./Windows/MessageWindow";
-// import { useGlitch } from "react-powerglitch";
+import { useGlitch } from "react-powerglitch";
 
 const Main = () => {
   const [zIndex, setZindex] = useState(1);
@@ -12,43 +12,44 @@ const Main = () => {
     setZindex((zIndex) => zIndex + 1);
   };
 
-  // const glitch = useGlitch({
-  //   createContainers: true,
-  //   hideOverflow: false,
-  //   timing: {
-  //     duration: 3250,
-  //     iterations: 1,
-  //   },
-  //   glitchTimeSpan: {
-  //     start: 0,
-  //     end: 1,
-  //   },
-  //   shake: {
-  //     velocity: 15,
-  //     amplitudeX: 0.2,
-  //     amplitudeY: 0.2,
-  //   },
-  //   slice: {
-  //     count: 6,
-  //     velocity: 15,
-  //     minHeight: 0.02,
-  //     maxHeight: 0.15,
-  //     hueRotate: true,
-  //   },
-  //   pulse: false,
-  // });
+  const glitch = useGlitch({
+    playMode: "hover",
+    createContainers: true,
+    hideOverflow: false,
+    timing: {
+      duration: 3250,
+      iterations: 1,
+    },
+    glitchTimeSpan: {
+      start: 0,
+      end: 1,
+    },
+    shake: {
+      velocity: 15,
+      amplitudeX: 0.2,
+      amplitudeY: 0.2,
+    },
+    slice: {
+      count: 6,
+      velocity: 15,
+      minHeight: 0.02,
+      maxHeight: 0.15,
+      hueRotate: true,
+    },
+    pulse: false,
+  });
 
   return (
     <main className="main">
       <div className="section__icons">
-        {/* <div className="test" ref={glitch.ref}>
+        <div className="test" ref={glitch.ref}>
           <img
             className="test__img"
             src="https://www.windows93.net/c/sys/skins/w93/documents.png"
             alt=""
           />
           <span>Documents</span>
-        </div> */}
+        </div>
       </div>
       <PopupWindow
         stackNote={stackNote}
@@ -84,9 +85,7 @@ const Main = () => {
         windowType={"image"}
         x={300}
         y={-150}
-      >
-        dsd
-      </MessageWindow>
+      ></MessageWindow>
     </main>
   );
 };
